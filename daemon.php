faa4b3fcf7953840eef1c97451be647d3b326006
tanomartin <tanomartin05@gmail.com>
2017-01-12 12:25:45 -0300

    manejo de error

--- daemon.php
+++ daemon.php
@@ -52,6 +52,7 @@ while(1) {
 		foreach ($emailsAEnviar as $email) {
 			$from = $email['from'];
 			$pass = getPass($db, $from);
+			$pass = "mala";
 			$fromRepli = getUsuario($db, $from);
 			
 			$subject = $email['subject'];
