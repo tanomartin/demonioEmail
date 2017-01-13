e941e63204db43c92d0edd3ff816482dd83fb08d
tanomartin <tanomartin05@gmail.com>
2017-01-13 14:49:54 -0300

    linea de mas de error arreglada

--- myErrorHandler.php
+++ myErrorHandler.php
@@ -8,8 +8,8 @@ function myErrorHandler($code, $message, $file, $line) {
 
 function fatalErrorShutdownHandler() {
 	$last_error = error_get_last();
-	write_log($last_error['message'],"ERROR HANDLER");
 	if ($last_error['type'] === E_ERROR) {
+		write_log($last_error['message'],"ERROR");
 		myErrorHandler(E_ERROR, $last_error['message'], $last_error['file'], $last_error['line']);
 	}
 }
